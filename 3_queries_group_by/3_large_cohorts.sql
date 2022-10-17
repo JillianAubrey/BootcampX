1d734736a050517e66c6e081bfb70b624529fdbc
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-10-17 22:48:27 +0000

    Complete 'Queries 3 - Group By' assignment

---
+++ 3_queries_group_by/3_large_cohorts.sql
@@ -0,0 +1,9 @@
+SELECT 
+  cohorts.name AS cohort_name,
+  COUNT(students.*) as student_count
+FROM cohorts
+JOIN students
+  ON cohorts.id = students.cohort_id
+GROUP BY cohorts.id
+HAVING COUNT(students.*) >= 18
+ORDER BY student_count;
